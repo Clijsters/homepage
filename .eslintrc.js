9cfaf0b3fd52cc0163d2849e0ec5fa263368a552
Christian Fischer <contact@cfischer-software.de>
2017-02-20 12:09:32 +0100

    Add eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,14 +8,30 @@ module.exports = {
   },
   env: {
     browser: true,
+    commonjs: true,
+    es6: true
   },
   // required to lint *.vue files
   plugins: [
     'html'
   ],
+  extends: '.eslintrc-default.js',
   // add your custom rules here
-  'rules': {
+  rules: {
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    semi: [2, 'never'],
+    'object-shorthand': [2, 'always'],
+    'id-length': 0,
+    'no-undef': 1,
+    indent: [2, 2, {
+      SwitchCase: 1,
+      VariableDeclarator: { var: 1, let: 1, const: 1 },
+      outerIIFEBody: 1,
+      MemberExpression: 1,
+      FunctionDeclaration: { parameters: 1 },
+      FunctionExpression: { parameters: 1 },
+      CallExpression: { arguments: 1 }
+    }]
   }
 }

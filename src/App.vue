008ca1df2ef96505fe9fd61ef362cca2e73310d7
Christian Fischer <contact@cfischer.io>
2017-04-01 11:39:50 +0200

    Add about section

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div id="app" class="app is__center">
     <navigation></navigation>
+    <about></about>
   </div>
 </template>
 
 <script>
 import navigation from 'components/Navigation'
+import about from 'components/About'
 export default {
   name: 'app',
   components: {
-    navigation
+    navigation,
+    about
   }
 }
 </script>
---
+++ src/components/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="about">
+    <img class="about__profile-picture" src="../assets/cfischer.jpg" />
+    <h1 class="about__hello">{{ $t("about.hello") }}</h1>
+    <p class="about__info">{{ $t("about.info") }}</p>
+  </section>
+</template>
+
+<script>
+export default { }
+</script>

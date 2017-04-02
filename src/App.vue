980c42a581a0200509e67e2d45fb34a74c0e9e04
Christian Fischer <contact@cfischer.io>
2017-04-02 19:11:41 +0200

    Add contact section

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app" class="app is__center">
     <navigation></navigation>
     <about></about>
+    <contact></contact>
   </div>
 </template>
 
 <script>
 import navigation from 'components/Navigation'
 import about from 'components/About'
+import contact from 'components/Contact'
 export default {
   name: 'app',
   components: {
     navigation,
-    about
+    about,
+    contact
   }
 }
 </script>
---
+++ src/components/Contact.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="contact">
+    <h1>{{ $t("contact.heading") }}</h1>
+    <p>{{ $t("contact.info") }}</p>
+    <ul>
+      <li class="contact__item">
+        <a href="mailto:contact@cfischer.io">
+          <img class="contact__image" src="../assets/mail.png" />
+          {{ $t("contact.mail") }}
+        </a>
+      </li>
+      <li class="contact__item">
+        <a href="https://www.linkedin.com/in/christianfischer1995" target="_blank">
+          <img class="contact__image" src="../assets/linkedin.png" />
+          {{ $t("contact.linkedin") }}
+        </a>
+      </li>
+      <li class="contact__item">
+        <a href="https://www.xing.com/profile/Christian_Fischer364" target="_blank">
+          <img class="contact__image" src="../assets/xing.png" />
+          {{ $t("contact.xing") }}
+        </a>
+      </li>
+      <li class="contact__item">
+        <a href="https://github.com/cfischer" target="_blank">
+          <img class="contact__image" src="../assets/github.png" />
+          {{ $t("contact.github") }}
+        </a>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default { }
+</script>

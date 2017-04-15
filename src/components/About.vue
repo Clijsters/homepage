eb1d2f3a7210e2afa3188e86b756aa7102de1657
Christian Fischer <contact@cfischer.io>
2017-04-15 23:48:18 +0200

    Add hero image

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="about">
-    <img class="about__profile-picture" src="../assets/cfischer.jpg" />
+    <img class="about__profile-picture" src="../assets/cfischer_header.jpg" />
     <h1 class="about__hello">{{ $t("about.hello") }}</h1>
     <p class="about__info">{{ $t("about.info") }}</p>
   </section>

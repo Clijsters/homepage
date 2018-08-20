ad980b2f8197380724a6490a2a5ced99d69a436a
Christian Fischer <christian.fischer@outfittery.de>
2018-08-20 19:50:36 +0200

    Update some translations and navbar width

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -4,8 +4,6 @@
       <li class="column--6 column-mobile--4 column-tablet--4 navigation__item navigation__item--first">
         <a href="#" class='navigation__link' :class="{ 'navigation__item--dark': isDark }" v-scroll-to="{ el: '#app' }">cfischer</a>
       </li>
-      <li class="column--1 navigation__item--offset">
-      </li>
       <li class="column--1 column-mobile--2 column-tablet--2 navigation__item">
         <a href="#" class='navigation__link' :class="{ 'navigation__item--dark': isDark }" v-scroll-to="{ el: '#about', offset: -40 }">{{ $t("navigation.about") }}</a>
       </li>

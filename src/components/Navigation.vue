f955834c2ae4be22517db06d50e72cd44aa82989
Christian Fischer <christian.fischer@outfittery.de>
2018-08-20 19:50:49 +0200

    Add hamburger menu

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,5 +1,30 @@
 <template>
-  <header class="container--fluid navigation">
+  <header class="header">
+    <a href="#" class='logo navigation__link' :class="{ 'navigation__item--dark': isDark }" v-scroll-to="{ el: '#app' }">cfischer</a>
+    <input class="menu-btn" type="checkbox" id="menu-btn" />
+    <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+    <ul class="menu">
+      <li>
+        <a href="#about" class='navigation__link' :class="{ 'navigation__item--dark': isDark }" v-scroll-to="{ el: '#about', offset: -40 }">{{ $t("navigation.about") }}</a>
+      </li>
+      <li>
+        <a href="#expertise" class='navigation__link' :class="{ 'navigation__item--dark': isDark }" v-scroll-to="{ el: '#skills', offset: -140 }">{{ $t("navigation.skills") }}</a>
+      </li>
+      <li>
+        <a href="#projects" class='navigation__link' :class="{ 'navigation__item--dark': isDark }" v-scroll-to="{ el: '#projects', offset: -140 }">{{ $t("navigation.work") }}</a>
+      </li>
+      <li>
+        <a href="#contact" class='navigation__link' :class="{ 'navigation__item--dark': isDark }" v-scroll-to="{ el: '#contact', offset: -110 }">{{ $t("navigation.contact") }}</a>
+      </li>
+      <li>
+        <button @click="changeLanguage"
+          :class="[{ 'navigation__change-language--en': isEnglish },
+          { 'navigation__change-language--de': isGerman }, 'navigation__change-language']">
+        </button>
+      </li>
+    </ul>
+  </header>
+  <!-- <header class="container--fluid navigation">
     <ul class="row navigation__list">
       <li class="column--6 column-mobile--4 column-tablet--4 navigation__item navigation__item--first">
         <a href="#" class='navigation__link' :class="{ 'navigation__item--dark': isDark }" v-scroll-to="{ el: '#app' }">cfischer</a>
@@ -23,7 +48,7 @@
         </button>
       </li>
     </ul>
-  </header>
+  </header> -->
 </template>
 
 <script>

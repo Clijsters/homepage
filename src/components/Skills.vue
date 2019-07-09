cc10b8f32e8ca280f448ce8d759abd17b9b9aa99
Christian Fischer <christian.fischer@outfittery.de>
2019-07-09 23:49:44 +0200

    add(cv): Add download cv to expertise section

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,6 +1,14 @@
 <template>
   <section id="skills" class="container--fluid skills">
     <h1 class='skills__heading'>{{ $t("skills.heading") }}</h1>
+    <ul class="skills__cv">
+      <li class="column--12 contact__item--dark">
+        <a href="https://cfischer.io/cv" target="_blank">
+          <img class="contact__image" src="../assets/cv_logo.png" />
+          <p class="contact__image__description">{{ $t("skills.downloadcv") }}</p>
+        </a>
+      </li>
+    </ul>
     <bar-chart :chart-data="chartData" :options="chartOptions"></bar-chart>
   </section>
 </template>

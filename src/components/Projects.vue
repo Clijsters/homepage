0d3e4bcfc9f1f7688727c2ef71ad8319e09d8b7f
Christian Fischer <christian.fischer@outfittery.de>
2019-07-09 17:04:38 +0200

    update(projects): Update project descriptions

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -10,8 +10,8 @@
           <h3 class="projects__header"><a href="https://outfittery.de/" class="projects__link" target="_blank">Outfittery</a></h3>
           <h4>Senior Frontend Engineer, Oct 2017 - present</h4>
           <h4>Berlin, Germany</h4>
-          <p>Outfittery is a curated fashion shopping service for men.</p>
-          <p>Currently my responsibilities include strategical technical planning, continuous development and maintenance of the product, technical leadership for customer facing applications, interviewing frontend and QA engineers as well as filling and prioritizing the backlog with product managers. Besides continuously improving the product and code base my key achievements are planning the migration to React, establishing a frontend user group, setting up a JS style guide in our projects, enabling the design team to create a design guide and introducing a new careers page in collaboration with contractors.</p>
+          <p>Outfittery is europe's leading personal shopping service for men. Outfittery combines Artificial Intelligence with real-life stylists to help customers find the right clothes. In 2018 Outfittery served 500,000 customers in eight European countries.</p>
+          <p>Currently my responsibilities include strategical technical planning, continuous development and maintenance of the product, technical leadership for customer facing applications frontend, interviewing frontend and QA engineers as well as filling and prioritizing the backlog with product managers. Besides continuously improving the product and code base my key achievements are planning the migration to React, establishing a frontend user group, setting up a JS style guide in our projects, enabling the design team to create a design guide and introducing a new careers page in collaboration with contractors.</p>
         </div>
       </div>
       <div class="row">
@@ -22,8 +22,8 @@
           <h3 class="projects__header"><a href="https://de.magine.com/" class="projects__link" target="_blank">Magine TV</a></h3>
           <h4>Software Engineer, Oct 2016 - Sep 2017</h4>
           <h4>Berlin, Germany</h4>
-          <p>Magine TV is an online television streaming platform.</p>
-          <p>In this role I ensured that the code base can live up to latest web standards and best practices. This included upgrading the AngularJS version, rewriting most services and introducing Webpack and SCSS. I coded a majority of the new landing page on which users watch TV. I was able to plan and manage the process of developing and releasing a new help center with our customer support. The marketing teams could rely on me to get help with IT questions, the maintenance of the Wordpress CMS and respective code changes.</p>
+          <p>Magine Germany operated an online television streaming platform which was discontinued in 2019. All customers were acquired by europe’s leading online TV platform Zattoo.</p>
+          <p>In this role I ensured that the code base can live up to latest web standards and best practices. This included upgrading the AngularJS version, rewriting most services and introducing Webpack and SCSS. I developed a majority of the page on which users watch TV. With the customer support team I was able to plan and release the Zendesk help desk. The marketing teams could rely on me to get help with IT questions, the maintenance of the Wordpress CMS and respective code changes.</p>
         </div>
       </div>
       <div class="row">
@@ -34,8 +34,8 @@
           <h3 class="projects__header"><a href="https://www.8man.com/en" class="projects__link" target="_blank">8MAN</a></h3>
           <h4>Software Engineer, Nov 2015 - Aug 2016</h4>
           <h4>Berlin, Germany</h4>
-          <p>8MAN is an access rights management enterprise solution.</p>
-          <p>This project gave me the opportunity to build this technically cutting edge web application. I made major technology and design decisions. Changes I introduced include rewriting and porting unstructured JavaScript code to TypeScript, unit testing with Jasmine and dropping the previous frontend framework in favor of Angular 2. I was also involved in the development of the C# ASP.NET Web API endpoints.</p>
+          <p>Leading European solution for work flow-based access rights management, offering permission analysis, security monitoring, documentation and reporting, and user provisioning. Enables companies and public authorities in protecting their IT systems by offering an easy-to-understand display, management and control of access and permission rights.</p>
+          <p>This project gave me the opportunity to build this technically cutting edge web application. I made major technology and design decisions. Changes I introduced include rewriting and porting unstructured JavaScript code to TypeScript, unit testing with Jasmine and dropping the previous frontend framework in favor of Angular 2. I was also involved in the development of the C\# ASP.NET Web API endpoints.</p>
         </div>
       </div>
       <div class="row">

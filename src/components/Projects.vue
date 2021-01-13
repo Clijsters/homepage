6c8ec7c7bab1ca0d494fc5a5103370bb8cc79d47
Dominique Clijsters <dominique.clijsters@gmail.com>
2021-01-13 17:51:48 +0100

    Further translation

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -16,9 +16,10 @@
           </h3>
           <h4>Senior Consultant, Nov 2020 - Now</h4>
           <h4>NRW/RLP, Germany</h4>
-          <p>DiSC is a fast-growing, young consulting startup in the Westerwald Area specialized on finance, telecommunication, and insurance.</p>
-          <p>With mostly technical consulting we have prominent customers in and around germany.</p>
-          <p>Currently, I'm supporting a company in the implementation of a modern micro service architecture for a nationwide network of traffic control, access control and monitoring systems. I design a modular system to enable the internal developers to easily and quickly create and roll out new services. This enables the customer to react quickly to market requirements and to outperform the competition.</p>
+          <!--          TODO: make them an array, iterate, finito.-->
+          <p>{{ $t("projects.disc.first") }}</p>
+          <p>{{ $t("projects.disc.second") }}</p>
+          <p>{{ $t("projects.disc.third") }}</p>
         </div>
       </div>
       <div class="row">
@@ -35,10 +36,10 @@
           </h3>
           <h4>Advanced Software Developer, Jan 2018 - Nov 2020</h4>
           <h4>Montabaur, Germany</h4>
-          <p>With 4.4 million customers, 1&1 Telecommunication SE is Germany's largest alternative DSL provider after Deutsche Telekom. Since 2010, 1&1 has also been operating as a successful mobile phone provider and in 2019 took one step further towards becoming a successful full-service provider with the acquisition of 5G licenses.</p>
-          <p>An important success factor is the sophisticated commission model of indirect sales, the technical development of which was my responsibility. Technical focus was on Java 11, Spring Boot, maven, Docker, grails, IBM ODM, postgreSQL and Sybase ASE.</p>
-          <p>In addition to technical development and expansion, constantly growing demands and changing regulations require quick reaction time, good concepts and excellent communication skills.</p>
-          <p>I also organized and participated in concern-wide tech-talks, Java User Group and supported the company on Job fairs, hackathons, etc.</p>
+          <p>{{ $t("projects.1und1.first") }}</p>
+          <p>{{ $t("projects.1und1.second") }}</p>
+          <p>{{ $t("projects.1und1.third") }}</p>
+          <p>{{ $t("projects.1und1.fourth") }}</p>
         </div>
       </div>
       <div class="row">
@@ -54,14 +55,15 @@
           </h3>
           <h4>Software Developer / CMS Consultant, Jul 2016 - Dec 2017</h4>
           <h4>Montabaur, Germany</h4>
-          <p>Chamaeleon AG is one of Germany's most successful CMS providers with focus on cities, municipalities and public organizations. With ionas4, a proprietary cms from Chamaeleon AG, the target group has been expanded to include medium-sized and large companies.</p>
-          <p>I was responsible for the further development of ionas4 and the development of SEO strategies for our customers. I also advised our corporate clients during the introduction of the new CMS and was technically responsible for migration projects. I also supervised apprentices and distributed tasks.</p>
+          <p>{{ $t("projects.chamaeleon.first") }}</p>
+          <p>{{ $t("projects.chamaeleon.second") }}</p>
+          <!--<p>{{ $t("projects.chamaeleon.third") }}</p>-->
         </div>
       </div>
       <div class="row">
         <div class="column--2 projects__offset" />
         <div class="projects__info column-mobile--12 column--8">
-          <img class="projects__logo projects__logo--man" src="../assets/datagroup_logo.svg">
+          <img class="projects__logo projects__logo--datagroup" src="../assets/datagroup_logo.svg" alt="Datagroup Logo">
           <h3 class="projects__header">
             <a
               href="https://www.datagroup.de"
@@ -72,9 +74,9 @@
           </h3>
           <h4>On-Site-Support & Software Development, Nov 2014 - Jul 2016</h4>
           <h4>Cologne Area, Germany</h4>
-          <p>As one of the leading IT service companies in Germany, DATAGROUP offers IT services and software solutions for many major customers throughout Germany.</p>
-          <p>During the on-site support at a large pharmaceutical company in NRW I was soon able to prove my extensive skills in the maintenance and development of software systems.</p>
-          <p>After being responsible for some hardware migrations, I soon dedicated myself to migrating outdated enterprise software to a modern Java technology stack. I also supported colleagues at other well-known customers in the extension of various SharePoint applications.</p>
+          <p>{{ $t("projects.datagroup.first") }}</p>
+          <p>{{ $t("projects.datagroup.second") }}</p>
+          <p>{{ $t("projects.datagroup.third") }}</p>
         </div>
       </div>
       <div class="row">
@@ -83,28 +85,30 @@
           <img
             class="projects__logo projects__logo--xylo-it"
             src="../assets/xylo-it_logo.png"
+            alt="Xylo-IT Logo"
           >
           <h3 class="projects__header">
             <a href="https://xylo-it.de" class="projects__link" target="_blank" rel="nofollow">Self employed</a>
           </h3>
           <h4>Software Developer / Web Consulting, Mar 2014 - present</h4>
           <h4>Cologne/Bonn Area, Germany</h4>
-          <p>Since 2014 I offer website and hosting consulting on freelance basis, with focus on rebranding and SEO. I offer complete hosting solutions and support my clients with GDPR and website performance issues.</p>
-          <p>I also sell handmade wooden cases for desktop computers.</p>
+          <p>{{ $t("projects.xylo-it.first") }}</p>
+          <p>{{ $t("projects.xylo-it.second") }}</p>
+          <!--<p>{{ $t("projects.xylo-it.third") }}</p>-->
         </div>
       </div>
       <div class="row">
         <div class="column--2 projects__offset" />
         <div class="projects__info column-mobile--12 column--8">
-          <img class="projects__logo projects__logo--agrip" src="../assets/agrip_logo.png">
+          <img class="projects__logo projects__logo--agrip" src="../assets/agrip_logo.png" alt="Agrip Logo">
           <h3 class="projects__header">
             <a href="https://agrip.de" class="projects__link" target="_blank" rel="nofollow">AGRIP.IT</a>
           </h3>
           <h4>IT System Administrator, Mar 2014 - Nov 2014</h4>
           <h4>Cologne/Bonn Area, Germany</h4>
-          <p>AGRIP is a local IT provider and expert for telephone systems, with a focus on small and medium-sized companies. AGRIP also offers postal and financial services as a partner of Deutsche Post AG.</p>
-          <p>My responsibility was the support of customers on site and the repair and maintenance of PC and printer systems. I was also responsible for supporting Deutsche Post customers, both in letter and parcel dispatch and in cash dispensing and deposits for Postbank customers. We have thus also managed high cash and stamp stocks.</p>
-          <p>I used this part-time position to have more time for the restoration of my Japanese youngtimer.</p>
+          <p>{{ $t("projects.agrip.first") }}</p>
+          <p>{{ $t("projects.agrip.second") }}</p>
+          <p>{{ $t("projects.agrip.third") }}</p>
         </div>
       </div>
       <div class="row">
@@ -121,15 +125,16 @@
           </h3>
           <h4>IT Data Scientist, Jun 2013 - Mar 2014</h4>
           <h4>Frankfurt, Germany</h4>
-          <p>Chinook deSign is a startup and think tank in Frankfurt, Germany, focusing on planning, testing and marketing online projects.</p>
-          <p>My role was to design and implement web applications based on Zend Framework 2, mainly in the areas of lead generation, insurance and comparison portals.</p>
-          <p>I was also responsible for all topics around SEO and web performance. I created my own onsite SEO concepts which resulted in performance improvements of several hundred percent. One of them even made a customer cry.</p>
+          <p>{{ $t("projects.chinook.first") }}</p>
+          <p>{{ $t("projects.chinook.second") }}</p>
+          <p>{{ $t("projects.chinook.third") }}</p>
         </div>
       </div>
       <div class="row">
         <div class="column--2 projects__offset" />
         <div class="projects__info column-mobile--12 column--8">
           <div class="projects__logo projects__logo--hmbuchdata" />
+          <img class="projects__logo projects__logo--buchdata" src="../assets/buchdata_logo.png" alt="H+M Buchdata Logo">
           <h3 class="projects__header">
             <a
               href="http://www.hmbuchdata.de"
@@ -138,11 +143,11 @@
               rel="nofollow"
             >H+M Buchdata EDV / Werner-von-Siemens-Schule</a>
           </h3>
-          <h4>IT-Systemelektroniker (Duale Berufsausbildung), Feb 2012 - Feb 2015</h4>
+          <h4>IT-Systemelektroniker (Duale Berufsausbildung), Aug 2010 - Jun 2013</h4>
           <h4>Frankfurt, Germany</h4>
-          <p>I completed my three-year professional training at HM Buchdata in Frankfurt, a system house with a broad customer base in the non-profit and public sectors and the economy.</p>
-          <p>During my training I learned how to work in a structured way, how to deal with customers in a professional manner and how to respect experience. This demanded a lot of patience from my trainer, as I found other skills more important at the age of 15.</p>
-          <p>However, my experience in programming enabled me to automate some internal processes, accelerate migrations and support my colleagues. After graduation I left the company immediately due to several moral concerns.</p>
+          <p>{{ $t("projects.buchdata.first") }}</p>
+          <p>{{ $t("projects.buchdata.second") }}</p>
+          <p>{{ $t("projects.buchdata.third") }}</p>
         </div>
       </div>
     </div>
